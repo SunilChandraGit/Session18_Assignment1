7bacfd1c4caaba4c02b2ab398fa0c89d34625950
neeil93 <chandrasunil93@gmail.com>
2018-07-02 21:38:30 +0530

    commit

---
+++ Cornstarch.py
@@ -0,0 +1,16 @@
+The population mean is 100.
+
+µ= 100
+
+Let us assume significance level as 5% (0.05).
+
+Random chance probability using z score and z-table.
+
+z= (108-100) / (15/Math.sqrt(36))
+
+z = 3.20
+
+Looking at z-table and p-value associated with 3.20 is 0.9993 i.e. probability of having value less than 108 is 0.9993 and 
+more than or equals to 108 is (1-0.9993)=0.0007.
+
+It is less than 0.05 so we will reject the Null hypothesis i.e. there is raw cornstarch effect.
